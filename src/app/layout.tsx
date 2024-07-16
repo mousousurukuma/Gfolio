bc900a74fc23df78642c7cd30614191ecf65db37
mousousurukuma <mousousurukuma@gmail.com>
2024-07-16 23:38:24 +0900

    fix vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
 import "./globals.css"
 import Header from "@/components/Header"
 import Sidebar from "@/components/Sidebar"
+import { ReactNode } from "react"
 
 export const metadata = {
   title: "Gfolio",
   description: "Gfolio Portfolio and Tracker",
 }
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className="bg-gray-900 text-white">
