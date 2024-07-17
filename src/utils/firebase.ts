f37607f69039d704892731e1a9d55c378bc3946e
mousousurukuma <mousousurukuma@gmail.com>
2024-07-17 22:47:07 +0900

    firebase Analytisc disable. build error collect

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage"
-import { getAnalytics } from "firebase/analytics"
+// import { getAnalytics } from "firebase/analytics"
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -17,6 +17,6 @@ const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
 const db = getFirestore(app)
 const storage = getStorage(app)
-const analytics = getAnalytics(app)
+// const analytics = getAnalytics(app)
 
 export { auth, db, storage }
